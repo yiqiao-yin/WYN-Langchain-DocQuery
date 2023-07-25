6f915d866baf904171abdc2d6e40fb08b5fa98ed
Yiqiao Yin <eagle0504@gmail.com>
2023-07-25 10:07:09 -0400

    csv added

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
 
 def generate_response_from_txt(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
@@ -21,6 +27,24 @@ def generate_response_from_txt(uploaded_file, openai_api_key, query_text):
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
+
+# Load CSV file
+def load_csv(input_csv):
+  df = pd.read_csv(input_csv)
+  with st.expander('See DataFrame'):
+    st.write(df)
+  return df
+
+
+# Generate LLM response
+def generate_response_from_csv(csv_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+  df = load_csv(csv_file)
+  # Create Pandas DataFrame Agent
+  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  # Perform Query using the Agent
+  response = agent.run(input_query)
+  return st.success(response)
 
 
 # Page title
@@ -50,3 +74,23 @@ with tab1:
 
     if len(result):
         st.info(response)
+
+with tab2:
+    # Input widgets
+    uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+    question_list = [
+    'How many rows are there?',
+    'What is the range of values for MolWt with logS greater than 0?',
+    'How many rows have MolLogP value greater than 0.',
+    'Other']
+    query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+
+    # App logic
+    if query_text is 'Other':
+        query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+        st.header('Output')
+        generate_response_from_csv(uploaded_file, query_text)
